3fa4abb357ae1fbbd1cf580bfda6b4aa5234f94c
Kamil Romanowicz <143507032+kemorsky@users.noreply.github.com>
2026-03-25 14:07:42 +0100

    move user fetch logic out of App.tsx

--- frontend/saas/src/App.tsx
+++ frontend/saas/src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { userQueryOptions, userSubscriptionQueryOptions } from './queries/userQueryOptions';
-import { signIn } from './api/api';
 import { Header } from './ui/blocks/shared/header';
 import { Hero } from './ui/blocks/home/hero';
 import { Features } from './ui/blocks/home/features';
@@ -12,19 +11,12 @@ import { ReadyToJoin } from './ui/blocks/home/ready-to-join';
 import { Footer } from './ui/blocks/shared/footer';
 
 export default function App() {
-  const queryClient = useQueryClient()
-
   const { data: user } = useQuery(userQueryOptions());
 
   const { data: user_subscription } = useQuery({
       ...userSubscriptionQueryOptions(), 
       enabled: !user
     });
-
-  const handleSignIn = async () => {
-    await signIn();
-    queryClient.invalidateQueries({ queryKey: ["user"] });
-  };
 
   return (
     <main className="max-w-360 w-full min-h-screen h-full bg-orange-200 flex flex-col justify-start items-center">
@@ -43,7 +35,6 @@ export default function App() {
       <div>
         {user_subscription?.id}
       </div>
-      <button onClick={() => handleSignIn()}>Sign In</button>
     </main>
   )
 }
--- frontend/saas/src/ui/blocks/shared/header.tsx
+++ frontend/saas/src/ui/blocks/shared/header.tsx
@@ -1,6 +1,19 @@
+import { Activity } from "react"
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { userQueryOptions } from '../../../queries/userQueryOptions';
+import { signIn } from '../../../api/api';
 import { Button } from "../../shared/buttons"
 
 export const Header = () => {
+    const queryClient = useQueryClient()
+
+    const { data: user, isLoading } = useQuery(userQueryOptions());
+
+    const handleSignIn = async () => {
+        await signIn();
+        queryClient.invalidateQueries({ queryKey: ["user"] });
+    };
+
     return (
         <header className="w-full bg-red-300 py-6 ">
             <nav className="w-full flex justify-between items-center">
@@ -29,8 +42,11 @@ export const Header = () => {
                     </section>
                 </section>
                 <section className="flex gap-4 px-4">
-                    <Button text="Sign In" variant="primary" />
+                    <Button onClick={() => handleSignIn()} text="Sign In" variant="primary" />
                     <Button text="Sign Up" variant="secondary" />
+                </section>
+                <section>
+                    <p>{user?.full_name}</p>
                 </section>
             </nav>
         </header>
