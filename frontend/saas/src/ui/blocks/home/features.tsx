e39cefee6b5e6ce8b19e35e9a838c772550b67c8
Kamil Romanowicz <143507032+kemorsky@users.noreply.github.com>
2026-05-21 15:22:31 +0200

    style: fix border issues in features

--- frontend/saas/src/ui/blocks/home/features.tsx
+++ frontend/saas/src/ui/blocks/home/features.tsx
@@ -5,27 +5,22 @@ import { FeaturesCard } from "../../components/cards/features-card"
 export const Features = () => {
     return (
         <Wrapper id="features" className="bg-dark">
-            <Container className="flex flex-col items-center justify-center border border-border">
-                {/* <h1 className="font-primary">Features</h1> */}
-                <section className="w-full sm:flex-row sm:flex flex-col items-center justify-center border-b border-border">
-                    <FeaturesCard className="sm:w-[60%] w-full sm:border-r border-b border-border"
+            <Container className="flex flex-col gap-[1px] items-center justify-center bg-border border border-transparent">
+                <section className="w-full sm:flex-row sm:flex flex-col gap-[1px] items-center justify-center">
+                    <FeaturesCard className="sm:basis-[60%] w-full"
                                 title="Test" 
-                                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                Labore nam, voluptate perspiciatis eius impedit quasi, quas quaerat maiores consequatur, commodi sit deserunt!"/>
-                    <FeaturesCard className="sm:w-[40%] w-full"
+                                description="Lorem ipsum dolor sit amet consectetur adipisicing elit."/>
+                    <FeaturesCard className="sm:basis-[40%] w-full"
                                 title="Test2" 
-                                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                Labore nam, voluptate perspiciatis eius impedit quasi, quas quaerat maiores consequatur, commodi sit deserunt!"/>
+                                description="Lorem ipsum dolor sit amet consectetur adipisicing elit."/>
                 </section>
-                <section className="w-full sm:flex-row sm:flex flex-col items-center justify-center">
-                    <FeaturesCard className="sm:w-[40%] w-full sm:border-r border-b border-border"
+                <section className="w-full sm:flex-row sm:flex flex-col gap-[1px] items-center justify-center">
+                    <FeaturesCard className="sm:basis-[40%] w-full"
                                 title="Test3" 
-                                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                Labore nam, voluptate perspiciatis eius impedit quasi, quas quaerat maiores consequatur, commodi sit deserunt!"/>
-                    <FeaturesCard className="sm:w-[60%] w-full"
+                                description="Lorem ipsum dolor sit amet consectetur adipisicing elit."/>
+                    <FeaturesCard className="sm:basis-[60%] w-full"
                                 title="Test4" 
-                                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                Labore nam, voluptate perspiciatis eius impedit quasi, quas quaerat maiores consequatur, commodi sit deserunt!"/>
+                                description="Lorem ipsum dolor sit amet consectetur adipisicing elit."/>
                 </section>
             </Container>
         </Wrapper>
--- frontend/saas/src/ui/components/cards/features-card.tsx
+++ frontend/saas/src/ui/components/cards/features-card.tsx
@@ -8,13 +8,13 @@ interface CardProps {
 
 export const FeaturesCard = (props: CardProps) => {
     return (
-        <div className={cn("bg-card p-4 max-h-68 h-full gap-3 w-full flex flex-col items-start justify-start", props.className)}>
+        <div className={cn("bg-card p-4 max-h-68 h-full gap-3 w-full flex flex-col items-start justify-start border border-transparent", props.className)}>
             <section className="flex gap-4 items-center font-primary text-text">
                 <h2 className="font-semibold text-xl">{props.title}</h2>
                 <p className="text-text">icon</p>
             </section>
-            <section className="text-left self-center">
-                <article className="mt-3 max-w-100">
+            <section className="text-left">
+                <article className="mt-3 w-full max-w-100">
                     <p className="font-secondary text-text-muted">{props.description}</p>
                 </article>
             </section>
